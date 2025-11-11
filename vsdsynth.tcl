e82fea7007432f5512507980aa7a79f8703bccee
Greg Phillips W4GAP <thinkitdata@users.noreply.github.com>
2025-11-11 15:10:27 -0500

    Create vsdsynth.tcl

---
+++ vsdsynth.tcl
@@ -0,0 +1,9 @@
+
+
+# Create variables
+puts "DesignName = $DesignName"
+puts "OutputDirectory = $OutputDirectory"
+puts "NetlistDirectory = $NetlistDirectory"
+puts "EarlyLibraryPath = $EarlyLibraryPath"
+puts "LateLibraryPath = $LateLibraryPath"
+puts "ConstraintsFile = $ConstraintsFile"
