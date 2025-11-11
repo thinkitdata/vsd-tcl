1722fdde3d3b76b4c5ee698f25f28ad140b415d9
Greg Phillips W4GAP <thinkitdata@users.noreply.github.com>
2025-11-11 15:40:39 -0500

    Fix error messages for file existence checks

--- vsdsynth.tcl
+++ vsdsynth.tcl
@@ -10,16 +10,31 @@ puts "ConstraintsFile = $ConstraintsFile"
 
 # Check if required directories and files as specified in csv exist
 if  {! [file exists $LateLibraryPath]} {
-  puts "\nError: cannot file late cell library in path $LateLibraryPath. Exiting..."
+  puts "\nError: cannot find late cell library in path $LateLibraryPath. Exiting..."
   exit
 } else {
     puts "\nInfo: Late cell library found in path $LateLibraryPath"
   }
 
 if {! [file isdirectory $OoutputDirectory]} {
-  puts "\nInfo: Cannot fine output directory $OutputDirectory. Creating $OutputDirectory"
+  puts "\nInfo: Cannot find output directory $OutputDirectory. Creating $OutputDirectory"
   file mkdir $OutputDirectory
 } else {
     puts "\nInfo: Output directory found in path $OutputDirectory"
   }
 
+if {! [file isdirectory $NetlistDirectory]} {
+  puts "\nInfo: Cannot find RTL netlist directory $NetlistDirectory. Exiting..."
+  exit
+} else {
+    puts "\nInfo: RTL netlist directory found in path $NetlistDirectory"
+  }
+
+if  {! [file exists $ConstraintsFile]} {
+  puts "\nError: cannot find constraints file in path $ConstraintsFile. Exiting..."
+  exit
+} else {
+    puts "\nInfo: Constraints file found in path $ConstraintsFile"
+  }
+
+  
