2f3240a4f1c6f5cdc7bc4e37422ac090801e1923
Greg Phillips W4GAP <thinkitdata@users.noreply.github.com>
2025-11-11 15:33:39 -0500

    Implement checks for required directories and files
    Added checks for the existence of late cell library and output directory.

--- vsdsynth.tcl
+++ vsdsynth.tcl
@@ -7,3 +7,19 @@ puts "NetlistDirectory = $NetlistDirectory"
 puts "EarlyLibraryPath = $EarlyLibraryPath"
 puts "LateLibraryPath = $LateLibraryPath"
 puts "ConstraintsFile = $ConstraintsFile"
+
+# Check if required directories and files as specified in csv exist
+if  {! [file exists $LateLibraryPath]} {
+  puts "\nError: cannot file late cell library in path $LateLibraryPath. Exiting..."
+  exit
+} else {
+    puts "\nInfo: Late cell library found in path $LateLibraryPath"
+  }
+
+if {! [file isdirectory $OoutputDirectory]} {
+  puts "\nInfo: Cannot fine output directory $OutputDirectory. Creating $OutputDirectory"
+  file mkdir $OutputDirectory
+} else {
+    puts "\nInfo: Output directory found in path $OutputDirectory"
+  }
+
